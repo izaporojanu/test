c9d695503375c70423facb9e7fabde6a1c40c07a
Igor Zaporojanu <izaporojanu@yahoo.com>
2016-08-11 13:31:00 +0300

    First test

---
+++ TestGITRepository/src/TestGitMainClass.java
@@ -0,0 +1,13 @@
+
+public class TestGitMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SecondClass sc = new SecondClass();
+		
+		System.out.println("Hello, GIT!");
+		sc.goodby();
+		System.out.println("Test no. 1.");
+	}
+
+}
